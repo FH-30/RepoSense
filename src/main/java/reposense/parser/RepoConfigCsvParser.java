d1785a2766087379984c1d31b6c0ddfd4b413253
FH-30 <finxictus@gmail.com>
2021-07-01 02:18:47 +0800

    Allow csv parsing of catching non pr commit config

--- src/main/java/reposense/parser/RepoConfigCsvParser.java
+++ src/main/java/reposense/parser/RepoConfigCsvParser.java
@@ -18,6 +18,7 @@ public class RepoConfigCsvParser extends CsvParser<RepoConfiguration> {
     public static final String REPO_CONFIG_FILENAME = "repo-config.csv";
     private static final String IGNORE_STANDALONE_CONFIG_KEYWORD = "yes";
     private static final String SHALLOW_CLONING_CONFIG_KEYWORD = "yes";
+    private static final String CATCH_NON_PR_COMMITS_CONFIG_KEYWORD = "yes";
 
     /**
      * Positions of the elements of a line in repo-config.csv config file
@@ -30,6 +31,7 @@ public class RepoConfigCsvParser extends CsvParser<RepoConfiguration> {
     private static final String IGNORE_COMMIT_LIST_CONFIG_HEADER = "Ignore Commit List";
     private static final String IGNORE_AUTHOR_LIST_CONFIG_HEADER = "Ignore Authors List";
     private static final String SHALLOW_CLONING_CONFIG_HEADER = "Shallow Cloning";
+    private static final String CATCH_NON_PR_COMMITS_CONFIG_HEADER = "Catch Non PR Commits";
 
     public RepoConfigCsvParser(Path csvFilePath) throws IOException {
         super(csvFilePath);
@@ -53,6 +55,7 @@ public class RepoConfigCsvParser extends CsvParser<RepoConfiguration> {
         return new String[] {
                 BRANCH_HEADER, FILE_FORMATS_HEADER, IGNORE_GLOB_LIST_HEADER, IGNORE_STANDALONE_CONFIG_HEADER,
                 IGNORE_COMMIT_LIST_CONFIG_HEADER, IGNORE_AUTHOR_LIST_CONFIG_HEADER, SHALLOW_CLONING_CONFIG_HEADER,
+                CATCH_NON_PR_COMMITS_CONFIG_HEADER,
         };
     }
 
@@ -98,10 +101,19 @@ public class RepoConfigCsvParser extends CsvParser<RepoConfiguration> {
                     "Ignoring unknown value " + shallowCloningConfig + " in shallow cloning column.");
         }
 
+        String catchingNonPrCommitsConfig = get(record, CATCH_NON_PR_COMMITS_CONFIG_HEADER);
+        boolean isCatchingNonPrCommitsPerformed =
+                catchingNonPrCommitsConfig.equalsIgnoreCase(CATCH_NON_PR_COMMITS_CONFIG_KEYWORD);
+
+        if (!isCatchingNonPrCommitsPerformed && !catchingNonPrCommitsConfig.isEmpty()) {
+            logger.warning(
+                    "Ignoring unknown value " + catchingNonPrCommitsConfig + " in catch non pr commits column.");
+        }
+
         RepoConfiguration config = new RepoConfiguration(
                 location, branch, formats, ignoreGlobList, isStandaloneConfigIgnored, ignoreCommitList,
                 isFormatsOverriding, isIgnoreGlobListOverriding, isIgnoreCommitListOverriding,
-                isShallowCloningPerformed);
+                isShallowCloningPerformed, isCatchingNonPrCommitsPerformed);
         config.setIgnoredAuthorsList(ignoredAuthorsList);
         config.setIsIgnoredAuthorsListOverriding(isIgnoredAuthorsListOverriding);
 
